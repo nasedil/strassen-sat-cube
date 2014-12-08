af0ee6745f43951aab1560e8a10a0a3e6e501595
Eugene Petkevich <genio@uib.no>
2014-12-08 07:23:44 +0100

    Update.

--- satmaker.py
+++ satmaker.py
@@ -35,6 +35,8 @@ class ConstraintCollector(object):
     def __init__(self):
         """Create a constraint collector."""
         self.constraints = []
+        self.xor_constraints = []
+        self.literals_count = 0
 
     def add(self, positive, negative):
         """Add constraint.
@@ -47,6 +49,21 @@ class ConstraintCollector(object):
         constraint["positive"] = positive
         constraint["negative"] = negative
         self.constraints.append(constraint)
+        self.literals_count = self.literals_count + len(positive) + len(negative)
+
+    def add_xor(self, positive, negative):
+        """Add xor constraint.
+
+        Arguments:
+        positive: a list of non-negated variables;
+        negative: a list of negated variables.
+        """
+        constraint = {}
+        constraint["positive"] = positive
+        constraint["negative"] = negative
+        self.xor_constraints.append(constraint)
+        self.literals_count = self.literals_count + len(positive) + len(negative)
+
 
 class SatPrinter(object):
 
@@ -62,12 +79,26 @@ class SatPrinter(object):
         self.vf = vf
         self.cc = cc
 
+    def print_statistics(self):
+        """Print problem statistics."""
+        print('Variables: ' + str(self.vf.count))
+        print('Clauses: ' + str(len(self.cc.constraints)))
+        print('Xor Clauses: ' + str(len(self.cc.xor_constraints)))
+        print('Literals: ' + str(self.cc.literals_count))
+
     def print(self, file):
         """Print SAT input into a file object in dimacs format."""
         file.write('p cnf ' +
                    str(self.vf.count) + ' ' +
-                   str(len(self.cc.constraints)) + '\n')
+                   str(len(self.cc.constraints) + len(self.cc.xor_constraints)) + '\n')
         for constraint in self.cc.constraints:
+            for variable in constraint['positive']:
+                file.write(str(variable["number"]) + ' ')
+            for variable in constraint['negative']:
+                file.write('-' + str(variable["number"]) + ' ')
+            file.write('0 \n')
+        for constraint in self.cc.xor_constraints:
+            file.write('x')
             for variable in constraint['positive']:
                 file.write(str(variable["number"]) + ' ')
             for variable in constraint['negative']:
